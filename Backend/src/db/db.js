ec7fad20f5fc9077dd0b443c0b49646151eab118
Krrish Vatshal <krrishvatshal2607@gmail.com>
2026-08-08 15:18:27 +0530

    feat: implement MongoDB connection setup

---
+++ Backend/src/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connectDB= async()=>{
+    try{
+        const conn=await mongoose.connect("");
+        console.log(`MongoDB Connected:"${conn.connection.host}`);
+    }
+    catch(error){
+        console.error("Error in connecting to MongoDB");
+        process.exit(1);
+    }
+}
+export default connectDB;
